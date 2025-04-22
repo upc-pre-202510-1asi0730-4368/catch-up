91a4b24948bd45d8eb3cbdec81c43efd42865128
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-22 18:05:02 -0500

    feat(sources): add source assembler.

---
+++ src/news/services/source.assembler.js
@@ -0,0 +1,23 @@
+import {LogoApiService} from "../../shared/logo-api.service.js";
+import {Source} from "../model/source.entity.js";
+
+const logoApi = new LogoApiService()
+
+export class SourceAssembler {
+    static toEntityFromResource(resource) {
+        let source = new Source({...resource});
+        source.urlToLogo = source.url !== '' ? logoApi.getUrlToLogo(source) : '';
+        return source;
+    }
+
+    static toEntitiesFromResponse(response) {
+        if (response.data.status !== 'ok') {
+            console.error(`${response.data.status}: ${response.data.message}`);
+            return [];
+        }
+        const sourcesResponse = response.data;
+        return sourcesResponse.sources.map((source) => {
+            return this.toEntityFromResource(source);
+        });
+    }
+}
