eac5bc23ff9c8aed759f6fc75aad1105d5aa528c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-22 18:38:30 -0500

    feat(sources): add source list component.

---
+++ src/news/components/source-list.component.vue
@@ -0,0 +1,34 @@
+<script>
+import {Source} from "../model/source.entity.js";
+import SourceItem from "./source-item.component.vue";
+
+export default {
+  name: "source-list",
+  components: {SourceItem},
+  emits: ['source-selected'],
+  props: { visible: Boolean, sources: Array[Source]},
+  data() {
+    return {
+      errors: []
+    }
+  },
+  methods: {
+    onSourceSelected(source) {
+      this.$emit('source-selected', source);
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-drawer v-bind:visible="visible">
+    <source-item v-for="source in sources"
+                 :key="source.id"
+                 :source="source"
+                 @source-selected="onSourceSelected(source)"/>
+  </pv-drawer>
+</template>
+
+<style scoped>
+
+</style>
